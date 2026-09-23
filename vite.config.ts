55fabeefe0c9fec14c83bceffbc23fe92699ed96
7asr5 <tttt.fbi.tttt@gmail.com>
2026-09-23 20:27:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/assistant-census/',
+  base: '/school-attendance-web/',
   plugins: [react()],
 })
 
